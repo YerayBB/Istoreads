1f600ed4d652d22665fae0788a881a73dbb8dec9
YerayBB <yeraybosoms@gmail.com>
2022-03-22 19:49:50 +0100

    vertex managing in poly implemented

--- Assets/Scripts/Polygon.cs
+++ Assets/Scripts/Polygon.cs
@@ -15,7 +15,7 @@ namespace Istoreads
         private float _radius;
         private bool[] _atachedVertex;
 
-        public static System.Action OnDestroyed;
+        public static event System.Action OnDestroyed;
 
 
         private void Awake()
@@ -29,6 +29,18 @@ namespace Istoreads
         void Start()
         {
 
+        }
+
+        private void Update()
+        {
+            Vector2[] path = new Vector2[_vertexAmount];
+            _lineRenderer.positionCount = _vertexAmount;
+            for(int i = 0; i < _vertexAmount; ++i)
+            {
+                path[i] = _transform.GetChild(i).localPosition;
+                _lineRenderer.SetPosition(i, path[i]);
+            }
+            _polygonCollider.SetPath(0, path);
         }
 
         private void FixedUpdate()
@@ -46,7 +58,7 @@ namespace Istoreads
             float angle = 2 * Mathf.PI / vertexAmount;
             Vector3 vertexPos;
             Vertex vertex;
-            for(int i = 0; i < _vertexAmount; i++)
+            for(int i = 0; i < _vertexAmount; ++i)
             {
                 vertexPos = new Vector3(Mathf.Cos(angle * i) * _radius, Mathf.Sin(angle * i) * _radius);
                 vertex = Spawner.Instance.GetVertex();
@@ -54,6 +66,7 @@ namespace Istoreads
                 _atachedVertex[i] = true;
                 vertex.Initialize(vertexPos, i, _transform);
             }
+            gameObject.SetActive(true);
         }
 
         private void DisableVertex(int vertexID)
@@ -68,25 +81,203 @@ namespace Istoreads
 
         private void CheckIntegrity()
         {
+            if(_vertexAmount == 0)
+            {
+                Death();
+            }
+            else if(_vertexAmount > 5)
+            {
+                List<Vector2Int> fragments = new List<Vector2Int>(); 
+                int start = -1;
+                //Detect Fragments
+                for(int i = 0; i < _initialVertex; ++i)
+                {
+                    if (_atachedVertex[i])
+                    {
+                        if (start != -1)
+                        {
+                            fragments.Add(new Vector2Int(start, i));
+                            start = -1;
+                        }
+                        else
+                        {
+                            start = i;
+                        }
+                    }
+                    else
+                    {
+                        if (start != -1)
+                        {
+                            fragments.Add(new Vector2Int(start, start));
+                            start = -1;
+                        }
+                    }
+                }
+                //check last and first are connected
+                if(fragments.Count > 1)
+                {
+                    if(_initialVertex - 1 - fragments[fragments.Count-1].y + fragments[0].x == 0)
+                    {
+                        fragments[0] = new Vector2Int(fragments[fragments.Count - 1].x, fragments[0].y);
+                        fragments.RemoveAt(fragments.Count - 1);
+                    }
+                }
 
+                //join small fragments
+                int numberFragments = fragments.Count;
+                int j = 0;
+                int vertexCount = 0;
+                Vector2Int target;
+                while (j < numberFragments) 
+                {
+                    //Detect how many active vertex contains the fragment
+                    target = fragments[j];
+                    vertexCount = 0;
+
+                    //case fragment includes end and origin vertex
+                    if (target.x > target.y)
+                    {
+                        for (int i = target.x; i < _initialVertex; ++i)
+                        {
+                            if (_atachedVertex[i]) ++vertexCount;
+                        }
+                        for (int i = 0; i <= target.y; ++i)
+                        {
+                            if (_atachedVertex[i]) ++vertexCount;
+                        }
+                    }
+                    else {
+
+                        for (int i = target.x; i <= target.y; ++i)
+                        {
+                            if (_atachedVertex[i]) ++vertexCount;
+                        }
+                    }
+
+                    //if fragment is small, join the fragent to the closest fragment
+                    if(vertexCount < 3)
+                    {
+                        int indexDown = (j - 1 + numberFragments) % numberFragments;
+                        int indexUp = (j + 1) % numberFragments;
+                        if (indexDown != indexUp)
+                        {
+                            if (Mathf.Abs(target.x - fragments[indexDown].y) <= Mathf.Abs(fragments[indexUp].x - target.y))
+                            {
+                                fragments[indexDown] = new Vector2Int(fragments[indexDown].x, target.y);
+                            }
+                            else
+                            {
+                                fragments[indexUp] = new Vector2Int(target.x, fragments[indexUp].y);
+                            }
+                        }
+                        else
+                        {
+                            fragments[indexDown] = new Vector2Int(fragments[indexDown].x, target.y);
+                        }
+                        fragments.RemoveAt(j);
+                        --numberFragments;
+                    }
+                    else
+                    {
+                        ++j;
+                    }
+                }
+
+                if(numberFragments > 1)
+                {
+                    //build the transform array
+
+                    Vector2Int range = fragments[1];
+                    int amount = 0;
+                    Transform[] vertexs = new Transform[_initialVertex];
+                    int subIndex = 0;
+
+                    //case range includes end and origin
+                    if (range.x > range.y)
+                    {
+                        for (int i = 0; i < _initialVertex; ++i)
+                        {
+                            if (_atachedVertex[i])
+                            {
+                                if (i >= range.y && i <= range.x)
+                                {
+                                    vertexs[i] = _transform.GetChild(subIndex);
+                                    ++amount;
+                                }
+                                else
+                                {
+                                    vertexs[i] = null;
+                                }
+                                ++subIndex;
+                            }
+                            else
+                            {
+                                vertexs[i] = null;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < _initialVertex; ++i)
+                        {
+                            if (_atachedVertex[i])
+                            {
+                                if (i >= range.x && i <= range.y)
+                                {
+                                    vertexs[i] = _transform.GetChild(subIndex);
+                                    vertexs[i].GetComponent<Vertex>().OnKilled -= DisableVertex;
+                                    ++amount;
+                                }
+                                else
+                                {
+                                    vertexs[i] = null;
+                                }
+                                ++subIndex;
+                            }
+                            else
+                            {
+                                vertexs[i] = null;
+                            }
+                        }
+                    }
+                    Spawner.Instance.GetPolygon().Split(amount, vertexs, _radius);
+
+                }
+            }
         }
 
-        public void Split(bool[] activeVertex, Transform[] vertexs, float radius)
+        public void Split(int amount, Transform[] vertexs, float radius)
         {
-            _initialVertex = vertexs.Length;
+            _initialVertex = amount;
             _vertexAmount = _initialVertex;
-            _atachedVertex = activeVertex;
+            _atachedVertex = new bool[_initialVertex];
             _radius = radius;
             float angle = 2 * Mathf.PI /_vertexAmount;
             Vector3 vertexPos;
             Vertex vertex;
-            for (int i = 0; i < _vertexAmount; i++)
+            int subIndex = 0;
+            for (int i = 0; i < vertexs.Length; ++i)
             {
                 vertexPos = new Vector3(Mathf.Cos(angle * i) * _radius, Mathf.Sin(angle * i) * _radius);
-                vertex = vertexs[i].GetComponent<Vertex>();
+                if(subIndex >= vertexs.Length)
+                {
+                    Debug.Log("Error in split, Transform array incomplete");
+                }
+                while(vertexs[subIndex] == null && subIndex < vertexs.Length)
+                {
+                    ++subIndex;
+                }
+                vertex = vertexs[subIndex].GetComponent<Vertex>();
                 vertex.OnKilled += DisableVertex;
                 vertex.Reatach(vertexPos, _transform);
             }
+            gameObject.SetActive(true);
+        }
+
+        private void Death()
+        {
+            gameObject.SetActive(false);
+            OnDestroyed?.Invoke();
         }
     }
 }
